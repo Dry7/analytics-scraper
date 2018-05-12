e17e400b70ee6f5a98dc42983b40807de9c7a3fd
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-12 21:06:35 +0300

    Register Country service and Scraper Service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\CountryService;
+use App\Services\ScraperService;
 use Illuminate\Support\ServiceProvider;
+use GuzzleHttp\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(CountryService::class);
+        $this->app->singleton(ScraperService::class, function () {
+            return new ScraperService(app(Client::class), config('analytics.backend_host'));
+        });
     }
 }
