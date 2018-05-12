c4d026e9ebe96bd52c469c6b6613bbe40d570261
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-12 21:07:03 +0300

    UpdateGroupJob

---
+++ app/Jobs/UpdateGroupJob.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\Html\VKService;
+use App\Services\ScraperService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+
+class UpdateGroupJob extends Job
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /** @var int */
+    private $networkId;
+
+    /** @var string */
+    private $url;
+
+    /** @var int */
+    public $tries = 3;
+
+    /** @var int  */
+    public $timeout = 60;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param int $networkId
+     * @param string $url
+     *
+     * @return void
+     */
+    public function __construct(int $networkId, string $url)
+    {
+        $this->networkId = $networkId;
+        $this->url = $url;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param VKService $service
+     * @param ScraperService $scraper
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function handle(VKService $service, ScraperService $scraper)
+    {
+        $data = $service->scraper($this->url);
+
+        if (!is_null($data)) {
+            $scraper->send($data);
+        }
+    }
+
+    public function fail($exception = null)
+    {
+        echo "\nFail - {$this->url} - $exception";
+    }
+}
