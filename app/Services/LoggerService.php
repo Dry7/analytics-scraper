7d2222aa6b24d57e48dec08aaff2e3f01c3e1750
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-26 11:27:55 +0300

    Logger provider

---
+++ app/Services/LoggerService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+
+class LoggerService
+{
+    private const REQUESTS = 'requests';
+
+    /** @var bool */
+    private $enabled = false;
+
+    /**
+     * LoggerService constructor.
+     * @param bool $enabled
+     */
+    public function __construct(bool $enabled = false)
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * @param string $networkCode
+     * @param array $data
+     */
+    public function log(string $networkCode, array $data): void
+    {
+        if (!$this->enabled) {
+            return;
+        }
+
+        if (!isset($data['source_id'])) {
+            $data['source_id'] = $this->getRandomID();
+        }
+
+        Storage::disk('local')->put(
+            self::REQUESTS . '/' . $networkCode . '/' . Carbon::now()->toDateString() . '/' . $data['source_id'] . '.txt',
+            json_encode($data, JSON_PRETTY_PRINT)
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getRandomID(): string
+    {
+        return $data['source_id'] = 'UNKNOWN_' . rand(1, 1000000000);
+    }
+}
